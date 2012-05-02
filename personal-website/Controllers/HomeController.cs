6107c0d6ec11c54003b983c36a1057cc2194b2cb
Sig. Tolleranza <haiku80@gmail.com>
2012-05-02 16:41:13 +0200

    Aggiornato CSS

--- personal-website/Controllers/HomeController.cs
+++ personal-website/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using personal_website.Controllers.Base;
 
 namespace personal_website.Controllers
@@ -11,7 +7,7 @@ namespace personal_website.Controllers
   {
     public ActionResult Index()
     {
-      ViewBag.Message = "This is just a test";
+      ViewBag.Message = "Agua de beber";
 
       return View();
     }
